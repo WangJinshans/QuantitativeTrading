7ba66bf5d21497d35ac9f0047014a71ffa32eaee
lance <wangjinshan@gmail.com>
2021-11-10 17:56:19 +0800

    feat: 新增当日数据录入mysql

--- db/store.go
+++ db/store.go
@@ -3,22 +3,23 @@ package db
 import (
 	"github.com/go-xorm/xorm"
 	"github.com/rs/zerolog/log"
+	"quant_trade/model"
 	"xorm.io/core"
 )
 
 var engine *xorm.Engine
 
 func init() {
-	//var err error
-	//engine, err = xorm.NewEngine("mysql", "root:wangjinshan@tcp(127.0.0.1:3306)/trade?charset=utf8")
-	//if err != nil {
-	//	log.Error().Msgf("connect to database error: %v", err)
-	//	return
-	//}
-	//err = engine.Sync2(new(model.StockInfo))
-	//if err != nil {
-	//	log.Error().Msgf("sync2 table error: %v", err)
-	//}
+	var err error
+	engine, err = xorm.NewEngine("mysql", "root:wangjinshan123..@tcp(127.0.0.1:3306)/trade?charset=utf8")
+	if err != nil {
+		log.Error().Msgf("connect to database error: %v", err)
+		return
+	}
+	err = engine.Sync2(new(model.StockInfo))
+	if err != nil {
+		log.Error().Msgf("sync2 table error: %v", err)
+	}
 }
 
 // 存储到mysql中
